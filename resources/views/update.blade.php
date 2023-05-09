05ef020aa576fba65acf0ae03904ef347b968ee5
Rustam <rustam@zestgeek.com>
2023-05-09 17:39:59 +0530

    updated update.blade file

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,1 +1,11 @@
-update.blade.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ci cd</title>
+</head>
+<body>
+
+</body>
+</html>
